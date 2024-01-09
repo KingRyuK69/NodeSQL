4b4086c8f13b80c9b24589212a4765dc59da5174
KingPuke <154311503+KingPuke@users.noreply.github.com>
2024-01-09 18:24:25 +0530

    changes_intial

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const app = express();
 
-//mysql route
+//mysql
 const routes = require("./routes/route");
-app.use("/api/users", routes);
 
-//squelize route
-const route1 = require("./routes/infoRoute");
-app.use("/api/users_info", route1);
+// //mysql route
+// const routes = require("./routes/route");
+// app.use("/api/users", routes);
+
+// //squelize route
+// const route1 = require("./routes/infoRoute");
+// app.use("/api/users_info", route1);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/", routes);
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
